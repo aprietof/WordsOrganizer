a905c03a6dcc08cf7d9068813ee8cac1e4ddeb13
aprietof <adrianprieto00@gmail.com>
2016-12-12 12:05:34 -0500

    initialize and len methods passing

--- words_organizer.rb
+++ words_organizer.rb
@@ -1,4 +1,21 @@
 class WordsOrganizer
 
+  attr_accessor :words
+
+  # It should initialize object with an empty words array attribute
+  def initialize(words = Array.new)
+    @words = words
+  end
+
+  # It should calculate WordsOrganizer words array's length
+  # It should return an integer
+  def len
+    length = 0
+    @words.each do |word|
+      length += 1
+    end
+    length
+  end
+
 
 end
